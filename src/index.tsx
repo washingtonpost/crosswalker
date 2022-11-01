949ad2bf0e1712e4f0463a1fb62b29e8dc795670
Dylan Freedman <freedmand@gmail.com>
2022-11-01 14:18:36 -0400

    chore: remove react-snap

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,17 @@
 import React from "react";
-import { hydrate, render } from "react-dom";
+import { render } from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const rootElement = document.getElementById("root");
-if (rootElement!.hasChildNodes()) {
-  hydrate(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    rootElement
-  );
-} else {
-  render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    rootElement
-  );
-}
+
+render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  rootElement
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
